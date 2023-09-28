f3413d01c7dc26daab036825e7f3df4169be54df
gseven-gdv <zerogravitystd@gmail.com>
2023-09-28 19:36:31 -0300

    init

--- Puffy-the-stars-killer/Scripts/PuffyTheStarsKiller.gd
+++ Puffy-the-stars-killer/Scripts/PuffyTheStarsKiller.gd
@@ -1,12 +1,27 @@
-extends Node2D
-
-@export var speed: int = 400
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+extends CharacterBody2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+const SPEED := 300.0
+const JUMP_VELOCITY := -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
