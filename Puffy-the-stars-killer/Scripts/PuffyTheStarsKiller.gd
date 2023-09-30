5e5b06e5b6aa63484ee320535c2df0d94ce95406
Vyctor Huggo <vyctorhuggo322@gmail.com>
2023-09-30 18:48:32 -0300

    play movimento foda

--- Puffy-the-stars-killer/Scripts/PuffyTheStarsKiller.gd
+++ Puffy-the-stars-killer/Scripts/PuffyTheStarsKiller.gd
@@ -1,27 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED := 300.0
-const JUMP_VELOCITY := -400.0
+const SPEED := 500.0
+const ACCELERATION := 1.0
+const FRICTION := 1.3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func _physics_process(delta) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	var direction = Input.get_axis("move_left", "move_right")
+func _physics_process(_delta: float) -> void:
+	_move()
+	# Handle Jump
+	# if Input.is_action_pressed("jump") and is_on_floor():
+	# velocity.y = JUMP_VELOCITY
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+
+func _move() -> void:
+	var direction: Vector2 = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
+	
+	if direction != Vector2.ZERO:
+		velocity.x = lerp(velocity.x, direction.normalized().x * SPEED, ACCELERATION)
+		velocity.y = lerp(velocity.y, direction.normalized().y * SPEED, ACCELERATION)
+		Transform2D.FLIP_X
+		return
+	
+	velocity.x = lerp(velocity.x, direction.normalized().x * SPEED, FRICTION)
+	velocity.y = lerp(velocity.y, direction.normalized().y * SPEED, FRICTION)
+	
+	
+	print(direction)
+
+	
