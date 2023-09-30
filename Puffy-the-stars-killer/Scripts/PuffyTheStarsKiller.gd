4f56394315746fe618437d422c1234dafe677608
Vyctor Huggo <vyctorhuggo322@gmail.com>
2023-09-30 19:04:38 -0300

    Update PuffyTheStarsKiller.gd

--- Puffy-the-stars-killer/Scripts/PuffyTheStarsKiller.gd
+++ Puffy-the-stars-killer/Scripts/PuffyTheStarsKiller.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-
-const SPEED := 500.0
+const SPEED := 400.0
 const ACCELERATION := 1.0
 const FRICTION := 1.3
 
@@ -10,10 +9,6 @@ var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(_delta: float) -> void:
 	_move()
-	# Handle Jump
-	# if Input.is_action_pressed("jump") and is_on_floor():
-	# velocity.y = JUMP_VELOCITY
-	
 	move_and_slide()
 
 func _move() -> void:
@@ -25,13 +20,10 @@ func _move() -> void:
 	if direction != Vector2.ZERO:
 		velocity.x = lerp(velocity.x, direction.normalized().x * SPEED, ACCELERATION)
 		velocity.y = lerp(velocity.y, direction.normalized().y * SPEED, ACCELERATION)
-		Transform2D.FLIP_X
 		return
 	
 	velocity.x = lerp(velocity.x, direction.normalized().x * SPEED, FRICTION)
 	velocity.y = lerp(velocity.y, direction.normalized().y * SPEED, FRICTION)
-	
-	
 	print(direction)
 
 	
