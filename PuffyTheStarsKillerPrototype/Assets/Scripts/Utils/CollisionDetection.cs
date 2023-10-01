8fe3ab21a6add4579fc1e95e1aba88d7b933c2b8
gseven-gdv <zerogravitystd@gmail.com>
2023-09-30 23:10:06 -0300

    init bevy kkkkk

---
+++ PuffyTheStarsKillerPrototype/Assets/Scripts/Utils/CollisionDetection.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+    public class CollisionDetection : MonoBehaviour
+    {
+        [Header("References")]
+        public LayerMask WhatIsBlocks;
+
+        [Header("Horizontal Collision")]
+        [SerializeField] private Vector2 _rightColOffset = Vector2.zero;
+        [SerializeField] private Vector2 _leftColOffset = Vector2.zero;
+
+        [Range(0f, 1f)]
+        [SerializeField] private float _horizontalColRadius = .5f;
+
+        [Header("Vertical Collision")]
+        [SerializeField] private Vector2 _bottomColSize = Vector2.zero;
+        [SerializeField] private Vector2 _bottomColOffset = Vector2.zero;
+
+        [Space]
+
+        public bool IsOnRightWall = false;
+        public bool IsOnLeftWall = false;
+        public bool IsOnWall = false;
+        public bool IsGroundedEarly = false;
+
+        public float BottomColYSize;
+
+        public int WallSide;
+
+        [Space]
+
+        public bool IsGrounded = false;
+
+        private void FixedUpdate()
+        {
+            IsOnRightWall = Physics2D.OverlapCircle((Vector2) transform.position + _rightColOffset, _horizontalColRadius, WhatIsBlocks);
+            IsOnLeftWall = Physics2D.OverlapCircle((Vector2) transform.position + _leftColOffset, _horizontalColRadius, WhatIsBlocks);
+
+            IsOnWall = IsOnLeftWall || IsOnRightWall;
+            WallSide = IsOnRightWall ? -1 : 1;
+
+            IsGrounded = Physics2D.OverlapBox((Vector2) transform.position + _bottomColOffset, _bottomColSize, 0f, WhatIsBlocks);
+            IsGroundedEarly = Physics2D.OverlapBox((Vector2) transform.position + new Vector2(_bottomColOffset.x, _bottomColOffset.y - .12f), new Vector2(_bottomColSize.x, _bottomColSize.y + .2f), 0f, WhatIsBlocks);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere((Vector2) transform.position + _rightColOffset, _horizontalColRadius);
+            Gizmos.DrawWireSphere((Vector2) transform.position + _leftColOffset, _horizontalColRadius);
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube((Vector2) transform.position + _bottomColOffset, _bottomColSize);
+        }
+    }
+}
---
+++ PuffyTheStarsKillerPrototype/Assets/Scripts/Utils/LookDir.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public static class LookDir
+    {
+        public static float GetDir(Vector3 self)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 targetPos = Camera.main.WorldToScreenPoint(self);
+
+            mousePos.x -= targetPos.x;
+            mousePos.y -= targetPos.y;
+
+            return Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        }
+
+        public static float GetDir(Vector3 self, Vector3 target)
+        {
+            Vector3 dir = target - self;
+
+            return Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        }
+
+        public static Vector3 GetDir(Vector3 target, bool returnVector3)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 targetPos = Camera.main.WorldToScreenPoint(target);
+
+            mousePos.x -= targetPos.x;
+            mousePos.y -= targetPos.y;
+
+            return mousePos;
+        }
+
+        public static Vector3 GetDir(Vector3 self, Vector3 target, bool returnVector2) => target - self;
+
+        public static Vector3 GetDir(float angle)
+        {
+            return new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Tan(angle * Mathf.Deg2Rad));
+        }
+
+        public static Vector3 AngleAxisToVector3(float angle, float rotationDistance) => Quaternion.AngleAxis(angle, Vector3.forward) * (Vector3.right * rotationDistance);
+    }
+}
