0339a92183a9a68e1f990cda4710cf66311e65f1
gseven-gdv <zerogravitystd@gmail.com>
2023-10-01 16:00:54 -0300

    -m

--- Dedededededestruction/puffy-the-universe-decimator/src/main.rs
+++ Dedededededestruction/puffy-the-universe-decimator/src/main.rs
@@ -1,66 +1,50 @@
-mod enemy;
-mod player;
-
 /// @author Gabriel Spinola - gabr
 ///
 /// References:
 /// LINK - https://bevy-cheatbook.github.io
 /// LINK - https://sburris.xyz/posts/bevy-gravity/
 /// LINK - https://www.youtube.com/watch?v=4TjEo-gDgAg&t=417
+/// LINK - https://github.com/jakobhellermann/bevy-inspector-egui
+/// LINK - Tuto: https://youtu.be/dlvXu18L828?si=gEMC8BiEIcgGJ5_F 
+/// LINK - UI: https://github.com/jkb0o/belly
 /// 
 /// LINK - CU: http://icts.unb.br/jspui/bitstream/10482/41315/1/2020_VicentedePaulaNascimentoLeiteFilho.pdf
+
+mod enemy;
+mod player;
+
+use std::default;
+
 use bevy::{app::AppExit, prelude::*, window::PrimaryWindow};
 use rand::random;
 
 use enemy::EnemyPlugin;
 use player::PlayerPlugin;
 
-#[derive(Resource)]
-pub struct StarSpawnTimerResource {
-    pub timer: Timer,
+#[derive(States, Debug, Default, Clone, Copy, Eq, PartialEq, Hash)]
+pub enum AppState {
+    MainMenu,
+    #[default]
+    Game,
+    GameOver,
 }
-
-impl Default for StarSpawnTimerResource {
-    fn default() -> Self {
-        StarSpawnTimerResource {
-            timer: Timer::from_seconds(STAR_SPAWN_TIME, TimerMode::Repeating),
-        }
-    }
-}
-
-pub fn enemy_hit_player(
-    mut commands: Commands,
-    mut player_query: Query<(Entity, &Transform), With<CPlayer>>, // entity's a u32 so we can clone it
-    mut game_over_event_write: EventWriter<GameOver>,
-    enemy_query: Query<&Transform, With<CEnemy>>,
-    score: Res<ScoreResource>,
-) {
-    if let Ok((player_entity, player_transform)) = player_query.get_single_mut() {
-        for enemy_transform in enemy_query.iter() {
-            let distance = player_transform
-                .translation
-                .distance(enemy_transform.translation);
-
-            let player_radius = PLAYER_SPRITE_SIZE / 2.0;
-            let enemy_radius = ENEMY_SPRITE_SIZE / 2.0;
-
-            // if the distance between two entities is minor than the sum of their radiuses
-            // they're colliding
-            if distance < player_radius + enemy_radius {
-                commands.entity(player_entity).despawn();
-                game_over_event_write.send(GameOver { score: score.value });
-            }
-        }
-    }
-}
-
-
-
-
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Puffy The Stars Killer".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+                .build(),
+        )
         .add_event::<GameOver>()
         .add_plugins((EnemyPlugin, PlayerPlugin))
         .run()
