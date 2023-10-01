964b22572ecc85aebd25a082f0aff5d19d7e608f
gseven-gdv <zerogravitystd@gmail.com>
2023-09-30 23:27:56 -0300

    movement

--- Dedededededestruction/puffy-the-universe-decimator/src/main.rs
+++ Dedededededestruction/puffy-the-universe-decimator/src/main.rs
@@ -3,29 +3,6 @@ use bevy::{prelude::*, window::PrimaryWindow};
 #[derive(Component)]
 pub struct Player {}
 
-pub fn spawn_player(
-    mut commands: Commands,
-    window_query: Query<&Window, With<PrimaryWindow>>,
-    asset_server: Res<AssetServer>,
-) {
-    let window: &Window = window_query.get_single().unwrap();
-
-    commands.spawn(
-        (
-            SpriteBundle {
-                transform: Transform::from_xyz(
-                    window.width() / 2.0,
-                    window.height() / 2.0,
-                    0.0
-                ),
-                texture: asset_server.load("sprites/purple.png"),
-                ..Default::default() // Set all other field to default
-            },
-            Player { },
-        ),
-    );
-}
-
 pub fn spawn_camera(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>
@@ -42,9 +19,68 @@ pub fn spawn_camera(
     });
 }
 
+pub fn spawn_player(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window: &Window = window_query.get_single().unwrap();
+
+    commands.spawn(
+        (
+            SpriteBundle {
+                transform: Transform::from_xyz(
+                    window.width() / 2.0,
+                    window.height() / 2.0,
+                    0.0
+                ).with_scale(Vec3::splat(0.3)),
+                texture: asset_server.load("sprites/purple.png"),
+                ..Default::default() // Set all other field to default
+            },
+            Player { },
+        ),
+    );
+}
+
+pub const PLAYER_SPEED: f32 = 500.0;
+
+pub fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+) {
+    // When we are not sure if our entity does'nt exist, we have to make sure that it's ok
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
+            direction += Vec3::new(-1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+
+        if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize()
+        }
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, (spawn_camera, spawn_player))
+        .add_systems(Update, player_movement)
         .run()
 }
