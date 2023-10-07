669c1cbe898ffc308bf3eb3c4db68dc1f7163168
gseven-gdv <zerogravitystd@gmail.com>
2023-10-06 22:13:29 -0300

    Update systems.rs

--- dededededestruction/src/entities/player/systems.rs
+++ dededededestruction/src/entities/player/systems.rs
@@ -1,7 +1,8 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+
 use crate::entities::components::{FrameTime, SpriteAnimation};
 
-use super::{components::PuffyTheStarsKiller, PLAYER_SPRITE_SIZE, PLAYER_SPEED};
+use super::{components::PuffyTheStarsKiller, PLAYER_SPEED, PLAYER_SPRITE_SIZE};
 
 pub fn spawn_puffy_stars_killer(
   mut commands: Commands,
@@ -41,30 +42,30 @@ pub fn confine_player(
   window_query: Query<&Window, With<PrimaryWindow>>,
 ) {
   if let Ok(mut transform) = player_query.get_single_mut() {
-      let window = window_query.get_single().unwrap();
+    let window = window_query.get_single().unwrap();
 
-      let half_player_size = PLAYER_SPRITE_SIZE / 2.0;
+    let half_player_size = PLAYER_SPRITE_SIZE / 2.0;
 
-      let x_min = 0.0 + half_player_size;
-      let x_max = window.width() - half_player_size;
-      let y_min = 0.0 + half_player_size;
-      let y_max = window.height() - half_player_size;
+    let x_min = 0.0 + half_player_size;
+    let x_max = window.width() - half_player_size;
+    let y_min = 0.0 + half_player_size;
+    let y_max = window.height() - half_player_size;
 
-      let mut translation = transform.translation;
+    let mut translation = transform.translation;
 
-      if translation.x < x_min {
-          translation.x = x_min;
-      } else if translation.x > x_max {
-          translation.x = x_max;
-      }
+    if translation.x < x_min {
+      translation.x = x_min;
+    } else if translation.x > x_max {
+      translation.x = x_max;
+    }
 
-      if transform.translation.y < y_min {
-          translation.y = y_min;
-      } else if translation.y > y_max {
-          translation.y = y_max;
-      }
+    if transform.translation.y < y_min {
+      translation.y = y_min;
+    } else if translation.y > y_max {
+      translation.y = y_max;
+    }
 
-      transform.translation = translation
+    transform.translation = translation
   }
 
   return;
@@ -73,31 +74,17 @@ pub fn confine_player(
 pub fn player_movement(
   keyboard_input: Res<Input<KeyCode>>,
   time: Res<Time>,
-  mut player_query: Query<&mut Transform, With<PuffyTheStarsKiller>>,
+  mut player_transform: Query<&mut Transform, With<PuffyTheStarsKiller>>,
 ) {
   // When we are not sure if our entity does'nt exist, we have to make sure that it's ok
-  if let Ok(mut transform) = player_query.get_single_mut() {
-      let mut direction = Vec3::ZERO;
+  if let Ok(mut transform) = player_transform.get_single_mut() {
+    if keyboard_input.any_pressed([KeyCode::Left, KeyCode::A]) {
+			transform.translation.x += -PLAYER_SPEED * time.delta_seconds();
+		}
 
-      if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::A) {
-          direction += Vec3::new(-1.0, 0.0, 0.0);
-      }
-
-      if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
-          direction += Vec3::new(1.0, 0.0, 0.0);
-      }
-
-      if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::W) {
-          direction += Vec3::new(0.0, 1.0, 0.0);
-      }
-
-      if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
-          direction += Vec3::new(0.0, -1.0, 0.0);
-      }
-
-      direction = direction.normalize_or_zero();
-
-      transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    if keyboard_input.any_pressed([KeyCode::Right, KeyCode::D])  {
+      transform.translation.x += PLAYER_SPEED * time.delta_seconds();
+    }
   }
 
   return;
